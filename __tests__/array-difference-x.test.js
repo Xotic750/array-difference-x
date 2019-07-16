66cffc62fa52a20cb7e1cc92ac9a7c4c2fe687b9
Graham Fairweather <xotic750@gmail.com>
2019-07-16 21:08:12 +0200

    Terraformed
    Auto build

--- __tests__/array-difference-x.test.js
+++ __tests__/array-difference-x.test.js
@@ -31,14 +31,12 @@ describe('difference', function() {
   });
 
   it('is a function', function() {
-    expect.assertions(1);
-    expect.assertions(1);
+    expect.assertions(1)/
     expect(typeof difference).toBe('function');
   });
 
   it('should return the difference of the given arrays', function() {
-    expect.assertions(1);
-    expect.assertions(1);
+    expect.assertions(1)/
     let actual = difference([1, 2, 3, 4, 5], [5, 2, 10]);
 
     expect(actual).toStrictEqual([1, 3, 4]);
@@ -49,14 +47,12 @@ describe('difference', function() {
   });
 
   it('should match `NaN`', function() {
-    expect.assertions(1);
-    expect.assertions(1);
+    expect.assertions(1)/
     expect(difference([1, NaN, 3], [NaN, 5, NaN])).toStrictEqual([1, 3]);
   });
 
   it('should work with large arrays', function() {
-    expect.assertions(1);
-    expect.assertions(1);
+    expect.assertions(1)/
     const array1 = new Array(LARGE_ARRAY_SIZE + 1).fill().map(Number.call, Number);
     const array2 = new Array(LARGE_ARRAY_SIZE).fill().map(Number.call, Number);
     const a = {};
@@ -70,8 +66,7 @@ describe('difference', function() {
   });
 
   it('should work with large arrays of objects', function() {
-    expect.assertions(1);
-    expect.assertions(1);
+    expect.assertions(1)/
     const object1 = {};
     const object2 = {};
     const largeArray = new Array(LARGE_ARRAY_SIZE).fill(object1);
@@ -80,15 +75,13 @@ describe('difference', function() {
   });
 
   it('should work with large arrays of `NaN`', function() {
-    expect.assertions(1);
-    expect.assertions(1);
+    expect.assertions(1)/
     const largeArray = new Array(LARGE_ARRAY_SIZE).fill(NaN);
     expect(difference([1, NaN, 3], largeArray)).toStrictEqual([1, 3]);
   });
 
   it('should ignore values that are not arrays or `arguments` objects', function() {
-    expect.assertions(1);
-    expect.assertions(1);
+    expect.assertions(1)/
     const args = (function() {
       return arguments;
     })(1, 2, 3);
