c3196586dee06f19d7c23fe4935bb6c492d1dd66
Graham Fairweather <xotic750@gmail.com>
2019-07-26 00:39:05 +0200

    :bookmark: v3.0.10
    Auto build

--- dist/array-difference-x.js
+++ dist/array-difference-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-24T17:03:30.847Z",
+  "date": "2019-07-25T22:38:58.741Z",
   "describe": "",
   "description": "Creates an array of array values not included in the other given arrays.",
   "file": "array-difference-x.js",
   "hash": "d88dc9565abe4d2441c7",
   "license": "MIT",
-  "version": "3.0.9"
+  "version": "3.0.10"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
