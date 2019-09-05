ccb4923a0279d5ee52c3ecc15d9db1a1b9386a6a
Graham Fairweather <xotic750@gmail.com>
2019-09-05 02:02:37 +0200

    Maintenance

--- dist/array-difference-x.js
+++ dist/array-difference-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-30T22:36:51.002Z",
+  "date": "2019-09-05T00:02:29.886Z",
   "describe": "",
   "description": "Creates an array of array values not included in the other given arrays.",
   "file": "array-difference-x.js",
